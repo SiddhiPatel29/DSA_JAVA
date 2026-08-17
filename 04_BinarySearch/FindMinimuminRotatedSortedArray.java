9f1748fce043203870adbf2c3b63a9460299a34a
CoreInventory Developer <siddhipatel1542@gmail.com>
2026-08-17 18:54:55 +0530

    Add rotated sorted array minimum solutions

---
+++ 04_BinarySearch/FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class FindMinimuminRotatedSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.print("Enter the elements of array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int result = findMinimum(arr);
+        System.out.println(result);
+    }
+    public static int findMinimum(int arr[]){
+        int low = 0, high = arr.length - 1;
+        int min = Integer.MAX_VALUE;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(arr[low] <= arr[high]){
+                min = Math.min(min, arr[low]);
+                break;
+            }
+
+            if(arr[low] <= arr[mid]){
+                min = Math.min(min, arr[low]);
+                low = mid + 1;
+            }
+            else{
+                min = Math.min(min, arr[mid]);
+                high = mid - 1;
+            }
+        }
+
+        return min;
+    }
+}
