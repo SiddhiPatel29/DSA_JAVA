48a325ddf04b22618c532f1d8ba5dcd1b925a86c
CoreInventory Developer <siddhipatel1542@gmail.com>
2026-08-27 20:59:49 +0530

    solve a problem using binary search

---
+++ 04_BinarySearch/KthElementofTwoSortedArrays.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class KthElementofTwoSortedArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n1 = sc.nextInt();
+
+        int arr1[] = new int[n1];
+        System.out.print("Enter the elements of first array: ");
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the size of array: ");
+        int n2 = sc.nextInt();
+
+        int arr2[] = new int[n2];
+        System.out.print("Enter the elements of second array: ");
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the kth element: ");
+        int k = sc.nextInt();
+
+        int result = findMedian(arr1, arr2, k);
+        System.out.print(result);
+    }
+    public static int findMedian( int arr1[], int arr2[], int k){
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        if(n1 > n2) return findMedian(arr2, arr1, k);
+        int low = Math.max(k-n2, 0), high = Math.min(n1, k);
+        int left = k;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = (left - mid1);
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if(mid1 < n1) r1 = arr1[mid1];
+            if(mid2 < n2) r2 = arr2[mid2];
+            if(mid1 - 1 >= 0) l1 = arr1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = arr2[mid2 - 1];
+            if(l1 <= r2 && l2 <= r1){
+                return Math.max(l1, l2);
+            }
+            else if(l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
+        }
+        return 0;
+    }
+}
